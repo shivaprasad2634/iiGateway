91afc312afab3d0071822f6db96b59e7d5505ad7
Kattula Shiva Prasad <shivakattula17@gmail.com>
2024-06-26 21:44:20 +0530

    inital project creation

---
+++ src/main/java/org/intelligence_integration/iiGateway/IiGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.intelligence_integration.iiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/intelligence_integration/iiGateway/IiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.intelligence_integration.iiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
